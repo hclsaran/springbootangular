e4123b2ba8a182481f398715fc95b83809a6d44a
hclsaran <renusaran@hotmail.com>
2022-10-25 12:13:37 -0400

    newly uploaded

---
+++ spring-boot-server/src/main/java/com/bezkoder/spring/jpa/h2/model/Tutorial.java
@@ -0,0 +1,64 @@
+package com.bezkoder.spring.jpa.h2.model;
+
+import javax.persistence.*;
+
+@Entity //whole objects as persistence
+@Table(name = "todo")
+public class Tutorial {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String title;
+
+	
+	private String description;
+
+	 
+	private boolean published;
+
+	public Tutorial() {
+
+	}
+
+	public Tutorial(String title, String description, boolean published) {
+		this.title = title;
+		this.description = description;
+		this.published = published;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+	public void setPublished(boolean isPublished) {
+		this.published = isPublished;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+	}
+
+}
